ab35828f4cde2fcf4b3a0719d62e6d403cef6784
kamil.grabczynski <kamil.grabczynski@bcf-software.com>
2023-02-08 22:25:01 +0100

    init database

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client';
+
+const users = [
+  {
+    id: 1,
+    role: 'basic',
+    name: 'Basic Test',
+    username: 'basic-test',
+    password: 'xdgwpeomr6_uNFpg17rr',
+  },
+  {
+    id: 434,
+    role: 'premium',
+    name: 'Premium Test',
+    username: 'premium-test',
+    password: 'GBLspofkqpfe_qwf34pomf',
+  },
+];
+
+const seed = async () => {
+  const prisma = new PrismaClient();
+
+  const existingUsers = await prisma.user.findMany();
+  const existingUsernames = existingUsers.map((eu) => eu.name);
+  const usersToCreate = users.filter(
+    (u) => !existingUsernames.includes(u.name),
+  );
+
+  await prisma.user.createMany({
+    data: usersToCreate,
+  });
+
+  await prisma.$disconnect();
+};
+
+seed();
